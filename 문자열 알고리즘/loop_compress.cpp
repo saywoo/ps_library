cb4e0f6724c5910d05557cef05c38b301c157d3e
saywoo <65606738+saywoo@users.noreply.github.com>
2023-03-29 04:26:52 +0900

    코드 수정

--- 문자열 알고리즘/loop_compress.cpp
+++ 문자열 알고리즘/loop_compress.cpp
@@ -25,7 +25,7 @@ void encode()
 
     // 문자열을 하나씩 훑어보며 압축
     for (int i = 0; i < len; i++) {
-        // 앞의 문자가 저장되어 있지 않다면
+        // 앞의 문자가 저장되어 있지 않다면 새로운 지정 문자 저장
         if (!before_char) {
             loop_count = 1;
             before_char = str[i];
@@ -40,12 +40,16 @@ void encode()
             if (loop_count > 1) {
                 // cache_str에 반복된 횟수를 문자열 형태로 저장
                 for (int temp_int = loop_count; temp_int; temp_int /= 10) {
-                    cache_str = (char)(loop_count % 10 + '0') + cache_str;
+                    cache_str = (char)(temp_int % 10 + '0') + cache_str;
                 }
 
                 // 반복되는 횟수를 문자열에 저장
                 res = res + cache_str;
-            } 
+            }
+
+            // 새로운 지정 문자 저장
+            loop_count = 1;
+            before_char = str[i];
         }
         // 앞의 문자와 지금의 문자가 같다면 loop_count를 하나 더해주기
         else {
@@ -79,5 +83,10 @@ void encode()
 
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    encode();
+
     return 0;
 }
